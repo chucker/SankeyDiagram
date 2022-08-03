b1ae6a381959d40287293f16bba6938622e42ffe
Sören Kuklau <chucker23n+github@gmail.com>
2022-08-04 01:17:45 +0200

    modernized projects; net45 to net472

--- Kant.Wpf.Controls.Chart.SankeyDiagram/Kant.Wpf.Controls.Chart.SankeyDiagram/Properties/AssemblyInfo.cs
+++ Kant.Wpf.Controls.Chart.SankeyDiagram/Kant.Wpf.Controls.Chart.SankeyDiagram/Properties/AssemblyInfo.cs
@@ -7,11 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Kant.Wpf.Controls.Chart.SankeyDiagram")]
 [assembly: AssemblyDescription("A powerful and easy to use WPF library for drawing Sankey diagram")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Kant.Wpf.Controls.Chart.SankeyDiagram")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -39,17 +35,3 @@ using System.Windows;
                                       //(used if a resource is not found in the page, 
                                       // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
